72cc8d314ee8dda97c6f9cf3046117ba8ce804ea
Jordan <jordie.earl@gmail.com>
2024-09-05 22:42:37 +0200

    Set up views and urls for product list template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Wine
+from datetime import datetime
 
-# Create your views here.
+
+def product_list(request):
+    """
+    View function to display a list of all wines.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered template with the list of wines.
+    """
+    wines = Wine.objects.all()  
+    context = {
+        'wines': wines,
+        'current_year': datetime.now().year,
+    }
+    return render(request, 'product_list.html', context)
