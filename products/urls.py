00fd59f297216e84eadfc26a72b43036674c8303
Jordan <jordie.earl@gmail.com>
2024-09-05 22:55:07 +0200

    Set up view and urls for product details template

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('<int:wine_id>/', views.product_details, name='product_details'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Wine
 from datetime import datetime
 
@@ -19,3 +19,11 @@ def product_list(request):
         'current_year': datetime.now().year,
     }
     return render(request, 'product_list.html', context)
+
+
+def product_details(request, wine_id):
+    """
+    View to display the details of a single wine item and handle adding it to user basket.
+    """
+    wine = get_object_or_404(Wine, id=wine_id)
+    return render(request, 'product_details.html', {'wine': wine})
