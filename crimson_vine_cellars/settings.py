b3c14201b1f955b314a3570e476ec3d8278ca2b8
Jordan <jordie.earl@gmail.com>
2024-09-05 22:41:46 +0200

    Create basic product list template

--- crimson_vine_cellars/settings.py
+++ crimson_vine_cellars/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import os
 
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
