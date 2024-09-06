0c82ec429f04e8b6fcf697e8d1e30b3ffcb61f90
Jordan <jordie.earl@gmail.com>
2024-09-06 15:55:11 +0200

    Create about us view and url path

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def home(request):
     """ A view to return the home page """
 
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """ A view to return the about us page """
+    
+    return render(request, 'about_us.html')
