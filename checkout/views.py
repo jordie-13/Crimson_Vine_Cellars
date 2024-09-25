f227f8926f3ef72db2924db50c5b16fbf3f80735
Jordan <jordie.earl@gmail.com>
2024-09-25 21:48:12 +0200

    Remove county field from admin and form files

--- checkout/views.py
+++ checkout/views.py
@@ -8,7 +8,7 @@ def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "You're bag is currently empty")
-        return redirect(reverse('products'))
+        return redirect(reverse('product_list'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
